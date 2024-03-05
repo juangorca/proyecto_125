b2141730336d4458c88e2932d7bdeb50ab6a783e
juangorca <140129726+juangorca@users.noreply.github.com>
2024-03-05 17:19:21 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+noseX = 0;
+noseY = 0;
+function preload() {
+clown_nose = loadImage('https://i.postimg.cc/yxmQTzVh/R.png');
+}
+function setup() {
+ canvas = createCanvas(300, 300);
+ canvas.center();
+ video = createCapture(VIDEO);
+ video.size(300,300);
+ video.hide();
+ poseNet = ml5.poseNet(video, modelLoaded);
+ poseNet.on('pose', gotPoses);
+ }
+function modelLoaded(){
+console.log("Posenet está inicializado");
+}
+function gotPoses(results){
+if(results.length > 0){
+console.log(results);
+noseX = results[0].pose.nose.x-40;
+ noseY = results[0].pose.nose.y;
+ }
+}
+function draw() {
+ image(video, 0, 0, 300, 300);
+image(clown_nose, noseX, noseY, 75, 35);
+}
+function take_snapshot(){ 
+save('miFotoConFiltro.png');
+}
+
